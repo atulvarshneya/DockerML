0ffab4288cbb6ff6b92dd7c5dbc7dd67ebbc6d4e
Atul Varshneya <atul.varshneya@gmail.com>
2018-05-14 07:50:10 -0700

    step towards including R

---
+++ build-images/install-irkernel.R
@@ -0,0 +1,6 @@
+install.packages(c('repr', 'IRdisplay', 'evaluate', 'crayon', 'pbdZMQ', 'devtools', 'uuid', 'digest'),repos = "http://cran.us.r-project.org")
+
+devtools::install_github('IRkernel/IRkernel')
+
+IRkernel::installspec()
+
